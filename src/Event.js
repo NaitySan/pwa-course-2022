c13719b99870dca452ead3626b030ba73d847885
NaitySan <nine_nine@outlook.com>
2022-02-21 14:26:32 +0700

    Changed logo, added new page

---
+++ src/Event.js
@@ -0,0 +1,13 @@
+import './css/Event.css';
+
+function Event(){
+    return(
+        <div>
+            <div className="ball">A</div>
+            <div className="ball">B</div>
+            <div className="ball">C</div>
+        </div>
+    )
+}
+
+export default Event;
--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App.js";
 import Profile from './Profile.js'
+import Event from "./Event.js";
 
 function Router(){
     return (
@@ -9,6 +10,7 @@ function Router(){
             <Routes>
                 <Route path="/" element={ <App/> } />
                 <Route path="/profile" element={<Profile/>}/>
+                <Route path='/event' element={<Event/>}/>
             </Routes>
         </BrowserRouter>
     )
