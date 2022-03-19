cba5e4ac945ec2cb7aeb60b14037cf0b538c41d8
NaitySan <nine_nine@outlook.com>
2022-03-19 10:33:42 +0700

    Finished exercise

--- src/components/Chatline.js
+++ src/components/Chatline.js
@@ -26,9 +26,14 @@ function Chatline (props) {
         .then(data => setElement(data.gender));
     } 
     else if (text.startsWith("/randomfact")) {
-      // ex10-01 here: Implement randomfact using fetch & promise
-      // https://uselessfacts.jsph.pl/random.json?language=en
-      
+      fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+      .then(res => res.json())
+      .then(data => setElement(data.text))
+    }
+    else if (text.startsWith("/bored")){
+      fetch("https://www.boredapi.com/api/activity")
+      .then(res => res.json())
+      .then(data => setElement(data.activity))
     }
   };
 
